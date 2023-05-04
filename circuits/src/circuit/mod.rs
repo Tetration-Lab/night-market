8a281607b9fa66f03f3fa39682938002ec32a7c4
Pakorn Nathong <pakorn.nth@hotmail.com>
2023-05-05 01:49:49 +0700

    docs: add circuit module func doc

--- circuits/src/circuit/mod.rs
+++ circuits/src/circuit/mod.rs
@@ -1,2 +1,6 @@
+/// The main circuit for the protocol, used to handle deposit, swap, lp, and withdraw.
 pub mod main;
+
+/// The migration circuit for the protocol, used to handle migration between the main circuit of
+/// set of fixed asset to another set of fixed asset.
 pub mod migration;

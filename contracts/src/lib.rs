20ff8707d82f0deff140256e720192992c18c537
Pakorn Nathong <pakorn.nth@hotmail.com>
2023-05-05 19:57:54 +0700

    feat: enforce neg on withdraw

--- contracts/src/lib.rs
+++ contracts/src/lib.rs
@@ -246,7 +246,7 @@ pub fn execute(
                     .map(|a| {
                         withdrawn_assets
                             .get(a)
-                            .map(|f| Fr::from_le_bytes_mod_order(&f.to_le_bytes()))
+                            .map(|f| Fr::from_le_bytes_mod_order(&f.to_le_bytes()).neg())
                             .unwrap_or_default()
                     })
                     .collect::<Vec<_>>(),

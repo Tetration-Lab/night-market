02c7bf3866f9c620e89491050a7b4e90d0e484e4
Pakorn Nathong <pakorn.nth@hotmail.com>
2023-05-08 19:48:35 +0700

    feat: add splitted circuit

--- circuits/src/circuit/mod.rs
+++ circuits/src/circuit/mod.rs
@@ -3,6 +3,9 @@
 /// The main circuit for the protocol, used to handle deposit, swap, lp, and withdraw. (or other supported actions)
 pub mod main;
 
+/// The main circuit, but splitted into two parts.
+pub mod main_splitted;
+
 /// The migration circuit for the protocol, used to handle migration between the main circuit of
 /// set of fixed asset to another set of fixed asset.
 pub mod migration;
